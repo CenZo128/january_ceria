15b646f9833f70e8dfb05c4587a3e35a610e9dfc
Vincent Sadino Guizot <vincentguizot128@gmail.com>
2021-01-16 14:15:50 +0700

    basic javascript

---
+++ basicJavascript.js
@@ -0,0 +1,43 @@
+//Variable -> sebuah penampung yang memiliki nilai/value
+
+var angka = 10;
+var greetings = "Hello, how are you ?";
+var isGood = true;
+
+var undef = undefined;
+var empty = null;
+
+//Tipe data primitif
+/**
+ * Number       : 1,2,3,100,1000,-10,-15,5.5,7.5
+ * String       : a,A,"hello world",'1',`backtick`
+ * Boolean      : true, false
+ * Object
+ * undefined
+ * null
+ */
+
+// console.log(angka);
+// console.log(greetings);
+// console.log(isGood);
+
+// console.log(undef);
+// console.log(empty);
+
+var alas = 5;
+var tinggi = 10;
+var luas;
+
+luas = 1/2 * alas * tinggi;
+
+// console.log(luas)
+// console.log("Jadi, hasil luas segitiga adalah : " + luas + " cm^2")
+
+var number = 10;
+var numString = '10';
+var kata1 = 'Hello';
+var kata2 = 'World'
+var nilai = true;
+
+console.log(nilai + number)
+console.log(kata1 + " " + kata2)
