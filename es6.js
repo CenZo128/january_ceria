acb03916e4f91386df2003f234f12fed3972e21f
Vincent Guizot <vincentguizot128@gmail.com>
2021-01-24 13:31:01 +0700

    add es6

---
+++ es6.js
@@ -0,0 +1,64 @@
+// ES6 -> ECMAScript 6 , keluar tahun 2015
+// JavaScript -> ECMAScript  
+// ES5 , keluar tahun 2009
+
+// Variable
+// ES5
+// console.log(angka);
+// var angka = 7;
+
+// ES6
+// let number = 10;
+
+// number = 12;
+// console.log(number);
+// let string = 'Makan';
+// console.log(string);
+
+// const mahasiswa = {
+//     name : "Vincent",
+//     gpa : 3.21,
+//     isGraduate : true
+// }
+// mahasiswa.gpa = 4.00
+
+// console.log(mahasiswa)
+
+// const laptops = ['ASUS','Dell']
+
+// laptops.push('HP')
+// // laptops = 'string'
+// console.log(laptops)
+
+// Function : Declaration, Expression, Arrow (ES6)
+// ES5
+
+// FUnction Declaration
+// greets()
+// function greets(){
+//     console.log("Hello!!")
+// }
+
+// Function Expression
+// greets()
+// const greets = function(){
+//     console.log("Expression FUnction")
+// }
+
+// ES6
+
+// Function Arrow 
+// greets()
+// const greets = () => {
+//     console.log("Arrow Function")
+// }
+
+
+/**
+ * Fitur - fitur ES6 :
+ * - High order function : .forEach, .map, .reduce, .filter
+ * - Destructuring Array, Destructuring Object
+ * - Class,
+ * - Promise (Callback)
+ * - dst
+ */
